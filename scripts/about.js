f751f94d575f369c7ba83ec22ac22d6a0844ee64
ABHIJOY SARKAR <asterdan712@gmail.com>
2020-04-08 09:38:04 +0530

    Image fix in about

--- scripts/about.js
+++ scripts/about.js
@@ -4,7 +4,7 @@ var i = 0;
 
 function randomColor(){
     i = (i+1)%noOfBackgrounds;
-	return "url(../assets/about_" + i.toString() + ".png)"+" center";
+	return "url(../assets/about_" + i.toString() + ".png)"+" no-repeat center center fixed";
 }
 
 function setColor(){
