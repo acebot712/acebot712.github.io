abce9f8cc5a86700ecaef5f47a08ba99c0cc0373
ABHIJOY SARKAR <asterdan712@gmail.com>
2020-04-22 23:24:07 +0530

    fixed broken links

--- scripts/blogScript.js
+++ scripts/blogScript.js
@@ -47,17 +47,6 @@ function setTitle(blogID){
 	heading.innerText = blogs[blogID].heading;
 }
 
-		/*
-		<div class="card" style="width:400px">
-		    <img class="card-img-top" src="../images/0_blog.jpg" alt="Card image" style="width:100%">
-		    <div class="card-body">
-		      <h4 class="card-title">John Doe</h4>
-		      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-		      <a href="#" class="btn btn-primary">See Profile</a>
-	    	</div>
-	    </div>
-	    */
-
 function populateBlogList(){
 	const entries = Object.entries(blogs)
 	var ul = document.getElementById("ul");
