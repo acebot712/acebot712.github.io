2cec9e9b214bd2b8b77529e0c9556eb4a708a1c2
ABHIJOY SARKAR <asterdan712@gmail.com>
2020-04-23 10:16:10 +0530

    responsive tested

--- scripts/about.js
+++ scripts/about.js
@@ -1,16 +1,16 @@
-// const noOfBackgrounds = 2;
-// var i = 0;
+const noOfBackgrounds = 2;
+var i = 0;
 
 
-// function randomColor(){
-//     i = (i+1)%noOfBackgrounds;
-// 	return "url(../assets/about_" + i.toString() + ".png)"+" no-repeat center center fixed";
-// }
+function randomColor(){
+    i = (i+1)%noOfBackgrounds;
+	return "url(../assets/about_" + i.toString() + ".png)"+" no-repeat center center fixed";
+}
 
-// function setColor(){
-// 	document.querySelector("html").style.background = randomColor();
-// 	setTimeout(setColor,10000);
-// }
+function setColor(){
+	document.querySelector("html").style.background = randomColor();
+	setTimeout(setColor,10000);
+}
 
-// //==============================DEFINITIONS ABOVE THIS LINE===========================================================================
-// setColor()
+//==============================DEFINITIONS ABOVE THIS LINE===========================================================================
+setColor()
