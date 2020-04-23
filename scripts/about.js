cc8e30e0b574d86ceaacd51d987829dc2cb09c1d
ABHIJOY SARKAR <asterdan712@gmail.com>
2020-04-23 10:21:54 +0530

    responsive tested

--- scripts/about.js
+++ scripts/about.js
@@ -8,7 +8,7 @@ function randomColor(){
 }
 
 function setColor(){
-	document.querySelector("body").style.background = randomColor();
+	document.querySelector("body").style.background-image = randomColor();
 	setTimeout(setColor,10000);
 }
 
